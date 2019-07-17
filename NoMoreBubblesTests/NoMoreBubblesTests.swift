06da0a66164fcd67953240cbd3d57d1ef0ea1b3f
Jason Jiang <jjiang1991@gmail.com>
2019-07-17 13:46:17 -0700

    Initial Commit

---
+++ NoMoreBubblesTests/NoMoreBubblesTests.swift
@@ -0,0 +1,34 @@
+//
+//  NoMoreBubblesTests.swift
+//  NoMoreBubblesTests
+//
+//  Created by Jason Jiang on 7/17/19.
+//  Copyright © 2019 Jason Jiang. All rights reserved.
+//
+
+import XCTest
+@testable import NoMoreBubbles
+
+class NoMoreBubblesTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
